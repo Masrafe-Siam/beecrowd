43681cb02b116b9ea188cbd54994c1c24725e828
MASRAFE BIN HANNAN SIAM <masrafesiammbhs1633@gmail.com>
2022-05-27 20:44:47 +0600

    Problem

---
+++ Problem/Problem 1005.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+    int main()
+    {
+        float A,B,x;
+        scanf("%f %f", &A,&B);
+        x=(A*3.5+B*7.5)/(3.5+7.5);
+        printf("MEDIA = %.5f\n",x);
+        return 0;
+    }
