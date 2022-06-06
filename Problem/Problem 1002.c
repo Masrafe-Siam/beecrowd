799d9d05df40eee19280daad2bab92e5c403a158
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 09:32:30 +0600

    The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:  Calculate the area using the formula given in the problem description.

--- Problem/Problem 1002.c
+++ Problem/Problem 1002.c
@@ -1,3 +1,8 @@
+/*The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+Calculate the area using the formula given in the problem description.*/
+
+
+
 #include<stdio.h>
 #define n 3.14159
     int main()
